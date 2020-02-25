426e5b75246b9a15dc5481e112c9445bd2c7e65c
Marcel <dolly.marcel@gmail.com>
2020-02-25 18:54:31 -0300

    Add WordOccurrencesIndexCreator

---
+++ WordOccurrencesIndexReader.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace gutenberg_analysis
+{
+    public class WordOccurrencesIndexReader : IDataReader<KeyValuePair<string, long>>
+    {
+        private readonly string path;
+
+        public WordOccurrencesIndexReader(string path)
+        {
+            this.path = path;
+        }
+
+        public IEnumerable<KeyValuePair<string, long>> Enumerate()
+        {
+            using var fileStream = File.OpenRead(path);
+            using var binaryReader = new BinaryReader(fileStream);
+
+            while (fileStream.Position < fileStream.Length)
+            {
+                var word = binaryReader.ReadString();
+                var firstOccurrenceOffset = binaryReader.ReadUInt64();
+                yield return new KeyValuePair<string, long>(word, (long)firstOccurrenceOffset);
+            }
+        }
+    }
+}
