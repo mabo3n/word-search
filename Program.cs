f1817e2763f475806d3e74f2a24875a5fd87ad36
Marcel <dolly.marcel@gmail.com>
2020-02-25 23:51:51 -0300

    Add Main menu scratch to Program.cs

--- Program.cs
+++ Program.cs
@@ -18,7 +18,93 @@ namespace GutenbergAnalysis
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            new Program().WordFrequencyAsync().Wait();
+            // new Program().WordFrequencyAsync().Wait();
+            new Program().Run();
+        }
+
+        private string ConsoleMenu(IEnumerable<string> text, Func<string, bool> validOptionFilter)
+        {
+            string input;
+
+            do
+            {
+                Console.WriteLine(string.Join('\n', text));
+
+                input = Console.ReadLine();
+            }
+            while(!validOptionFilter(input.Trim()));
+
+            return input.Trim();
+        }
+
+        private string MainMenu()
+        {
+            var text = new[]
+            {
+                "What you wanna do?",
+                "(1) Build indexes",
+                "(2) Search for a word",
+                "(0) Exit",
+            };
+            var options = new[] {"0", "1", "2"};
+
+            return ConsoleMenu(text, input => options.Contains(input));
+        }
+
+        private string IndexesMenu()
+        {
+            var text = new[]
+            {
+                "Which index do you want to [re]build?",
+                "(1) Word Ocurrences Index (higher layer)",
+                "(2) Word Ocurrences Index (higher layer) and Word Ocurrences (lower layer)",
+                "(0) Exit",
+            };
+            var options = new[] {"0", "1", "2"};
+
+            return ConsoleMenu(text, input => options.Contains(input));
+        }
+
+        private string WordSearchMenu()
+        {
+            var text = new[]
+            {
+                "Type the word you want to search for, or 0 to exit:",
+            };
+
+            return ConsoleMenu(text, input => true);
+        }
+
+        private void Run()
+        {
+            string input;
+
+            if ((input = MainMenu()) != "0")
+            {
+                if (input == "1")
+                {
+                    if ((input = IndexesMenu()) != "0")
+                    {
+                        if (input == "1")
+                        {
+                            // build higher layer index
+                            Run();
+                        }
+                        else if (input == "2")
+                        {
+                            // build lower and higher layer index
+                            Run();
+                        }
+                    } else Run();
+                }
+                else if (input == "2")
+                {
+                    if ((input = WordSearchMenu()) != "0")
+                    {
+                        // execute search for input
+                    } else Run();
+                }
+            }
         }
 
         public IEnumerable<string> GetFilePaths(string root)
