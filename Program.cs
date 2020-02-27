ef72e5e78fa11b612cf894b9c4edae3fc0fead44
Marcel <dolly.marcel@gmail.com>
2020-02-27 19:37:01 -0300

    Implement main menu

--- Program.cs
+++ Program.cs
@@ -15,18 +15,26 @@ namespace GutenbergAnalysis
     {
         Dictionary<string, int> Frequencies = new Dictionary<string, int>();
         ulong TotalWordCount = 0;
-        public static string Root = @"C:\Users\rober\RiderProjects\word-search\data";
-        public static string DatabasePath = @"C:\Users\rober\RiderProjects\word-search\db.txt";
-        public static string DatabaseIndexPath = @"C:\Users\rober\RiderProjects\word-search\db_indexes.txt";
+        public static string SourcePath = "data/";
+        public static string DatabasePath = "index_db/db.txt";
+        public static string DatabaseIndexPath = "index_db/db_indexes.txt";
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            //new WordOccurrenceDatabase(Root, DatabasePath).Read();
-            var wordIndexes = new WordIndexes(DatabasePath, DatabaseIndexPath);
-            wordIndexes.Create();
-            Console.WriteLine("Reading");
-            wordIndexes.Read();
+            new Program().Run();
+        }
+
+        static void TimedExecution(Action action)
+        {
+            var timer = Stopwatch.StartNew();
+            try
+            {
+                action();
+            }
+            finally
+            {
+                Console.WriteLine($"Executed in {timer.Elapsed.ToString()}.");
+            }
         }
 
         private string ConsoleMenu(IEnumerable<string> text, Func<string, bool> validOptionFilter)
@@ -35,7 +43,9 @@ namespace GutenbergAnalysis
 
             do
             {
+                Console.WriteLine();
                 Console.WriteLine(string.Join('\n', text));
+                Console.WriteLine();
 
                 input = Console.ReadLine();
             }
@@ -44,71 +54,82 @@ namespace GutenbergAnalysis
             return input.Trim();
         }
 
-        private string MainMenu()
-        {
-            var text = new[]
-            {
-                "What you wanna do?",
-                "(1) Build indexes",
-                "(2) Search for a word",
-                "(0) Exit",
-            };
-            var options = new[] {"0", "1", "2"};
-
-            return ConsoleMenu(text, input => options.Contains(input));
-        }
-
-        private string IndexesMenu()
-        {
-            var text = new[]
-            {
-                "Which index do you want to [re]build?",
-                "(1) Word Ocurrences Index (higher layer)",
-                "(2) Word Ocurrences Index (higher layer) and Word Ocurrences (lower layer)",
-                "(0) Exit",
-            };
-            var options = new[] {"0", "1", "2"};
-
-            return ConsoleMenu(text, input => options.Contains(input));
-        }
-
-        private string WordSearchMenu()
-        {
-            var text = new[]
-            {
-                "Type the word you want to search for, or 0 to exit:",
-            };
-
-            return ConsoleMenu(text, input => true);
-        }
-
-        private void Run()
+        public void Run()
         {
             string input;
 
+            Func<string> MainMenu = () => ConsoleMenu(
+                new[] {
+                    "What you wanna do?",
+                    "(1) Build indexes",
+                    "(2) Search for a word",
+                    "(0) Exit",
+                },
+                input => new[] {"0", "1", "2"}.Contains(input)
+            );
             if ((input = MainMenu()) != "0")
             {
                 if (input == "1")
                 {
+                    Func<string> IndexesMenu = () => ConsoleMenu(
+                        new[] {
+                            "Which index do you want to [re]build?",
+                            "(1) Word Indexes (higher layer)",
+                            "(2) Word Indexes (higher layer) and Word Ocurrences Database (lower layer)",
+                            "(0) Exit",
+                        },
+                        input => new[] {"0", "1", "2"}.Contains(input)
+                    );
+
                     if ((input = IndexesMenu()) != "0")
                     {
                         if (input == "1")
                         {
-                            // build higher layer index
+                            if (!File.Exists(DatabasePath))
+                            {
+                                Console.WriteLine("\nWord Occurrences Database not found!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Building Word Indexes...");
+                                TimedExecution(
+                                    new WordIndexes(DatabasePath, DatabaseIndexPath).Create
+                                );
+                            }
+
                             Run();
                         }
                         else if (input == "2")
                         {
-                            // build lower and higher layer index
+                            Console.WriteLine("Building Word Occurrence Database...");
+                            TimedExecution(
+                                new WordOccurrenceDatabase(SourcePath, DatabasePath).Create
+                            );
+
+                            Console.WriteLine("Building Word Indexes...");
+                            TimedExecution(
+                                new WordIndexes(DatabasePath, DatabaseIndexPath).Create
+                            );
+
                             Run();
                         }
                     } else Run();
                 }
                 else if (input == "2")
                 {
+                    Func<string> WordSearchMenu = () => ConsoleMenu(
+                        new[] {
+                            "Type the word you want to search for, or 0 to exit:",
+                        },
+                        input => true
+                    );
+
                     if ((input = WordSearchMenu()) != "0")
                     {
                         // execute search for input
+                        Console.WriteLine($"Searching for \"{input}\"...");
+
+                        Run();
                     } else Run();
                 }
             }
@@ -194,7 +215,7 @@ namespace GutenbergAnalysis
             var timer = Stopwatch.StartNew();
             Console.WriteLine("Starting");
 
-            foreach (var filePath in GetFilePaths(Root))
+            foreach (var filePath in GetFilePaths(SourcePath))
             {
                 await executionBlock.SendAsync(filePath);
             }
